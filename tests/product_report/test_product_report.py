cf4fef774fc6eab2af437b5afe47ab3872af2921
Robert Rodrigues <robert.rm131@gmail.com>
2022-10-28 19:15:08 -0300

    feat: tests simple report implementation from product entity

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+import io
+import sys
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    expected_report = """O produto farinha fabricado em 01-05-2021 por Farinini com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."""
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "any",
+        "ao abrigo de luz",
+    )
+    my_test_stream = io.StringIO()
+    with io.StringIO() as my_stream:
+        print(product, file=my_stream, end="")
+        print(expected_report, file=my_test_stream, end="")
+        assert my_stream.getvalue() == my_test_stream.getvalue()
