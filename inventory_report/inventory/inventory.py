d4ec33efc7d37202c1f5f7abf53aa28f381e3a07
Robert Rodrigues <robert.rm131@gmail.com>
2022-10-29 18:30:27 -0300

    feat: json support for Inventory report

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,23 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
     def import_data(cls, path, relatory_type):
         products_list = []
-        with open(path, mode="r", newline="") as file:
-            reader = csv.DictReader(file)
-            products_data = [row_as_dict for row_as_dict in reader]
-            products_list = products_data
+        if ".json" in path:
+            with open(path, mode="r", newline="") as file:
+                products_data = json.load(file)
+                products_list = products_data
+        elif ".csv" in path:
+            with open(path, mode="r", newline="") as file:
+                reader = csv.DictReader(file)
+                products_data = [row_as_dict for row_as_dict in reader]
+                products_list = products_data
+
         if relatory_type.title() == "Simples":
             return SimpleReport.generate(products_list)
         elif relatory_type.title() == "Completo":
