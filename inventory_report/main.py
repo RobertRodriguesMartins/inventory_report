ca39da1984682a1a428554a3458397a54fa6a9e2
Robert Rodrigues <robert.rm131@gmail.com>
2022-10-31 20:26:18 -0300

    feat: main invetory module ready for accept shell args

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import io
+import sys
+
+
 def main():
-    pass
+    try:
+        relatory_path = sys.argv[1]
+        relatory_type = sys.argv[2]
+        strategies = [CsvImporter, JsonImporter, XmlImporter]
+        for strategy in strategies:
+            instance = InventoryRefactor(strategy)
+            try:
+                report = instance.import_data(relatory_path, relatory_type)
+                print(report, end="")
+            except ValueError:
+                if isinstance(instance, XmlImporter):
+                    sys.exit("invalid file format")
+    except IndexError:
+        print("Verifique os argumentos", file=sys.stderr)
