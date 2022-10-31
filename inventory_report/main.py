2e6ec4d3df1348b37772a7e26bcdc094942ea658
Robert Rodrigues <robert.rm131@gmail.com>
2022-10-31 20:33:25 -0300

    attempt to fix linter issues

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -2,8 +2,18 @@ from inventory_report.inventory.inventory_refactor import InventoryRefactor
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
-import io
 import sys
+
+
+def report_generator(strategies, relatory_path, relatory_type):
+    for strategy in strategies:
+        instance = InventoryRefactor(strategy)
+        try:
+            report = instance.import_data(relatory_path, relatory_type)
+            print(report, end="")
+        except ValueError:
+            if isinstance(instance, XmlImporter):
+                sys.exit("invalid file format")
 
 
 def main():
@@ -11,13 +21,6 @@ def main():
         relatory_path = sys.argv[1]
         relatory_type = sys.argv[2]
         strategies = [CsvImporter, JsonImporter, XmlImporter]
-        for strategy in strategies:
-            instance = InventoryRefactor(strategy)
-            try:
-                report = instance.import_data(relatory_path, relatory_type)
-                print(report, end="")
-            except ValueError:
-                if isinstance(instance, XmlImporter):
-                    sys.exit("invalid file format")
+        report_generator(strategies, relatory_path, relatory_type)
     except IndexError:
         print("Verifique os argumentos", file=sys.stderr)
